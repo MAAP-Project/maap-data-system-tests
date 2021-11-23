671404c15b32980aaa1f773c4c4396ef6223a791
Phil Varner <philvarner@gmail.com>
2021-11-22 22:22:57 -0500

    make pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -Eeuo pipefail
 set -x # print each command before executing
 
-papermill system-tests.ipynb system-tests-out.ipynb -p my_param 2
+papermill system-tests.ipynb system-tests-out.ipynb -p my_param 1
