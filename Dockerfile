e4103bd533f6db92090a4eb76ae59f7af03d1e82
Phil Varner <phil@philvarner.com>
2021-11-22 22:24:31 -0500

    Basic pipeline (#2)
    workflow with private github action docker container using ADE image

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM mas.ops.maap-project.org/root/jupyter-image/vanilla:develop
+
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+set -x # print each command before executing
+
+papermill system-tests.ipynb system-tests-out.ipynb -p my_param 1
