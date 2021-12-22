67fecab8f7016b3b9bed12aa9d9ecde781435c05
Phil Varner <philvarner@gmail.com>
2021-12-22 09:05:50 -0500

    handle PR refs correctly

--- harness.py
+++ harness.py
@@ -3,36 +3,44 @@ import glob
 import re
 import argparse
 
+
 def main():
 
     parser = argparse.ArgumentParser(
         description="Execute the test Jupyter Notebooks.")
-    parser.add_argument("--stage", required=True, help="stage to execute against")
+    parser.add_argument("--stage", required=True,
+                        help="stage to execute against")
     args = parser.parse_args()
 
     success_results = []
     failure_results = {}
 
-    for filename in [f for f in glob.glob('./*.ipynb') if not f.endswith("-output.ipynb")]:
-      try:
-        pm.execute_notebook(
-          filename,
-          re.sub("^(.*)\.ipynb$", r"\1-output.ipynb", filename),
-          parameters=dict(stage=args.stage),
-          progress_bar=False
-        )
-        success_results.append(filename)
-      except pm.exceptions.PapermillExecutionError as e:
-        failure_results[filename] = e
+    stage = args.stage
+
+    # Pull Requests should be tested against the dit env
+    if re.fullmatch(r"refs/pull/\d+/merge", stage):
+        stage = "dit"
+
+    for filename in [f for f in glob.glob("./*.ipynb") if not f.endswith("-output.ipynb")]:
+        try:
+            pm.execute_notebook(
+                filename,
+                re.sub("^(.*)\.ipynb$", r"\1-output.ipynb", filename),
+                parameters={"stage": stage},
+                progress_bar=False
+            )
+            success_results.append(filename)
+        except pm.exceptions.PapermillExecutionError as e:
+            failure_results[filename] = e
 
     if failure_results:
-      print("Failures:")
-      for (k, v) in failure_results.items():
-        print(f"File {k} => {v}")
-      exit(1)
+        print("Failures:")
+        for (k, v) in failure_results.items():
+            print(f"File {k} => {v}")
+        exit(1)
     else:
-      exit(0)
+        exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/maap_test_helper.py
+++ tests/maap_test_helper.py
@@ -4,13 +4,13 @@ from typing import Union, Any, Tuple
 
 
 def populate_maap_api_host(stage):
-    if stage == 'main':
-        env_prefix = ''
-    if stage == 'staging':
-        env_prefix = 'staging.'
-    elif stage == 'production':
-        env_prefix = 'ops.'
-    return f'api.{env_prefix}maap-project.org'
+    if stage == "production":
+        env_prefix = "ops."
+    elif stage in ["main", "dit"]:
+        env_prefix = ""
+    else:
+        env_prefix = f"{stage}."
+    return f"api.{env_prefix}maap-project.org"
 
 
 def fetch_results(maap, collection={}, query={}, timeout=180):
@@ -27,13 +27,13 @@ def fetch_results(maap, collection={}, query={}, timeout=180):
 
 def try_test_actual(test, actual, expected: Union[str, int, Tuple[Any, Any]]):
     try:
-      if isinstance(expected, str) or isinstance(expected, int):
-          assert actual == expected
-      elif isinstance(expected, Tuple):
-        if expected[0]:
-            assert actual >= expected[0]
-        if expected[1]:
-            assert actual <= expected[1]
+        if isinstance(expected, str) or isinstance(expected, int):
+            assert actual == expected
+        elif isinstance(expected, Tuple):
+            if expected[0]:
+                assert actual >= expected[0]
+            if expected[1]:
+                assert actual <= expected[1]
     except Exception as e:
         print(
             f"{test}: {actual} does not match bounds {expected}")
