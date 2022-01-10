86a8958051dae5dd5a1f63a900efe090419acdde
Phil Varner <phil@philvarner.com>
2022-01-10 12:17:52 -0500

    CMR Tests and test infrastructure improvements (#7)
    * test helper functions refactor
    * test infrastructure improvements
    * handle PR refs correctly
    * add CMR tests

--- harness.py
+++ harness.py
@@ -2,37 +2,51 @@ import papermill as pm
 import glob
 import re
 import argparse
+import os
+
 
 def main():
 
     parser = argparse.ArgumentParser(
         description="Execute the test Jupyter Notebooks.")
-    parser.add_argument("--stage", required=True, help="stage to execute against")
+    parser.add_argument("--stage", required=True,
+                        help="stage to execute against")
     args = parser.parse_args()
 
     success_results = []
     failure_results = {}
 
-    for filename in [f for f in glob.glob('./*.ipynb') if not f.endswith("-output.ipynb")]:
-      try:
-        pm.execute_notebook(
-          filename,
-          re.sub("^(.*)\.ipynb$", r"\1-output.ipynb", filename),
-          parameters=dict(stage=args.stage),
-          progress_bar=False
-        )
-        success_results.append(filename)
-      except pm.exceptions.PapermillExecutionError as e:
-        failure_results[filename] = e
+    stage = args.stage
+
+    # Pull Requests should be tested against the dit env
+    if re.fullmatch(r"refs/pull/\d+/merge", stage):
+        stage = "dit"
+
+    output_path_prefix = os.environ.get("GITHUB_WORKSPACE", "")
+
+    for input_path in [f for f in glob.glob("./*.ipynb") if not f.endswith("-output.ipynb")]:
+        try:
+            output_path_filename = re.sub(
+                " ^ (.*)\.ipynb$", r"\1-output.ipynb", input_path)
+            output_path = f"{output_path_prefix}/{output_path_filename}"
+            pm.execute_notebook(
+                input_path=input_path,
+                output_path=output_path,
+                parameters={"stage": stage},
+                progress_bar=False,
+            )
+            success_results.append(input_path)
+        except pm.exceptions.PapermillExecutionError as e:
+            failure_results[input_path] = e
 
     if failure_results:
-      print("Failures:")
-      for (k, v) in failure_results.items():
-        print(f"File {k} => {v}")
-      exit(1)
+        print("Failures:")
+        for (k, v) in failure_results.items():
+            print(f"File {k} => {v}")
+        exit(1)
     else:
-      exit(0)
+        exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ tests/maap_test_helper.py
@@ -0,0 +1,44 @@
+from difflib import SequenceMatcher
+import json
+from typing import Union, Any, Tuple, Iterable
+
+
+def populate_maap_api_host(stage):
+    if stage == "production":
+        env_prefix = "ops."
+    elif stage in ["main", "dit"]:
+        env_prefix = ""
+    else:
+        env_prefix = f"{stage}."
+    return f"api.{env_prefix}maap-project.org"
+
+
+def fetch_results(maap, collection={}, query={}, timeout=180):
+    """
+    Function which utilizes the `executeQuery` function to return the results of queries.
+    """
+    return json.loads(maap.executeQuery(
+        src=collection,
+        query=query,
+        poll_results=True,
+        timeout=timeout
+    ).text)
+
+
+def assert_results(test: str, actual: Iterable[Any], expected: Union[str, int, Tuple[Any, Any]]) -> None:
+    try:
+        if isinstance(expected, str) or isinstance(expected, int):
+            assert len(actual) == expected
+        elif isinstance(expected, Tuple):
+            if expected[0]:
+                assert len(actual) >= expected[0]
+            if expected[1]:
+                assert len(actual) <= expected[1]
+    except Exception as e:
+        print(
+            f"{test}: {len(actual)} does not match bounds {expected}")
+        raise e
+
+
+def similar(a, b) -> float:
+    return SequenceMatcher(None, a, b).ratio()
