70cfcb90c3819eae87ce6d1e0407580a7bfcbe69
Phil Varner <philvarner@gmail.com>
2022-01-27 12:12:52 -0500

    fix biomass dashboard ui tests when running against prod

--- cypress/integration/biomass-dashboard-ui.spec.js
+++ cypress/integration/biomass-dashboard-ui.spec.js
@@ -22,16 +22,19 @@ describe('biomass dashboard ui', () => {
   })
 
   it('should have populated the Products and Country Pilots menus', () => {
+
+    // these use the ends with operator "$=" because in DIT, the URLs are off of the root (/),
+    // whereas in Earthdata deployments for staging and prod they're off of /maap-biomass    
     cy.get(
-      'header > div > nav > div > div > ul > li > section > ul > li > a[href="/products/global"] > span '
+      'header > div > nav > div > div > ul > li > section > ul > li > a[href$="/products/global"] > span '
     ).should('have.text', 'All (Global)')
 
     cy.get(
-      'header > div > nav > div > div > ul > li > section > ul > li > a[href="/products/cci_biomass"] > span '
+      'header > div > nav > div > div > ul > li > section > ul > li > a[href$="/products/cci_biomass"] > span '
     ).should('have.text', 'CCI Biomass 2020')
 
     cy.get(
-      'header > div > nav > div > div > ul > li > section > ul > li > a[href="/country_pilots/japan"] > span '
+      'header > div > nav > div > div > ul > li > section > ul > li > a[href$="/country_pilots/japan"] > span '
     ).should('have.text', 'Japan')
   })
 
