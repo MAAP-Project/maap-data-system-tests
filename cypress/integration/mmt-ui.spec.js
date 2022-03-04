a1738f484c9293c20d0d1115091576f84cc78d39
Alex Bush <alexbush@developmentseed.org>
2022-03-04 16:35:59 +0000

    feat: add second cypress test; chore: tidy

--- cypress/integration/mmt-ui.spec.js
+++ cypress/integration/mmt-ui.spec.js
@@ -2,7 +2,6 @@
 
 describe('mmt ui', () => {
   beforeEach(() => {
-
     const url = (() => {
       const stage = Cypress.env('STAGE')
       if (stage == "production")
@@ -14,18 +13,18 @@ describe('mmt ui', () => {
     cy.visit(url)
   })
 
-
   it('should have the correct title', () => {
     cy.title().should('eq', 'Metadata Management Tool')
   })
 
-  it('should have populated the about box', () => {
+  it('should have populated the about MMT box', () => {
+    cy.get('#main-content > main > div > div > section:nth-child(1) > h2')
+      .should('have.text', 'About the Metadata Management Tool')
+  })
 
-    // these use the ends with operator "$=" because in DIT, the URLs are off of the root (/),
-    // whereas in Earthdata deployments for staging and prod they're off of /maap-biomass    
-    cy.get(
-      '#main-content > main > div > div > section:nth-child(1) > h2'
-    ).should('have.text', 'About the Metadata Management Tool')
+  it('should have populated the about CMR box', () => {
+    cy.get('#main-content > main > div > div > section:nth-child(2) > h2')
+      .should('have.text', 'About the CMR')
   })
 
 })
