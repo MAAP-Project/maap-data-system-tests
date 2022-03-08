234ce4adedbbb9230ca4f256c0cee70695d8ed59
Alex Bush <alexbush@developmentseed.org>
2022-03-08 12:58:04 +0000

    feat: add UMF tests

---
+++ cypress/integration/umf-ui.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('UMF (questionaire) ui', () => {
+  beforeEach(() => {
+    const url = (() => {
+      const stage = Cypress.env('STAGE')
+      if (stage == "production")
+        return "https://questionaire.maap-project.org"
+      else if (["main", "dit"].includes(stage) || stage.startsWith("refs"))
+        return "https://questionaire.dit.maap-project.org"
+    })();
+
+    cy.visit(url)
+  })
+
+  it('should have the correct title', () => {
+    cy.title().should('eq', 'MAAP User Shared Data Questionnaire')
+  })
+
+  it('should have a visible Start button, () => {
+    cy.get('body > div > div > div > div > div.details_holder > div > div:nth-child(1) > button')
+      .should('have.text', 'Start New')
+  })
+
+  it('should have populated blurb', () => {
+    cy.get('body > div > div > div > div > div.details_holder > p:nth-child(1)')
+      .should('have.text', 'Thank you for sharing your data with the MAAP community! As a reminder, data shared through this process should be provided by you. You are responsible for any content you upload to the MAAP and please do not post content that isn’t your own.')
+  })
+
+})
