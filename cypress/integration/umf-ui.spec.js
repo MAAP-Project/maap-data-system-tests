ebc83f3a399826404aec7f89f7fdff77fa0d8edd
Alex Bush <alexbush@developmentseed.org>
2022-03-08 15:27:11 +0000

    feat: text contains

--- cypress/integration/umf-ui.spec.js
+++ cypress/integration/umf-ui.spec.js
@@ -20,12 +20,12 @@ describe('UMF (questionaire) ui', () => {
 
   it('should have a visible Start button', () => {
     cy.get('body > div > div > div > div > div.details_holder > div > div:nth-child(1) > button')
-      .should('have.text', 'Start New')
+      .contains('Start New')
   })
 
   it('should have populated blurb', () => {
     cy.get('body > div > div > div > div > div.details_holder > p:nth-child(1)')
-      .should('have.text', 'Thank you for sharing your data with the MAAP community! As a reminder, data shared through this process should be provided by you. You are responsible for any content you upload to the MAAP and please do not post content that isn’t your own.')
+      .contains('Thank you for sharing your data with the MAAP community! As a reminder, data shared through this process should be provided by you. You are responsible for any content you upload to the MAAP and please do not post content that isn’t your own.')
   })
 
 })
